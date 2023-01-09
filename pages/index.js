f9ab1c4b16c1ef99be22740cd8cc658a0ea9860e
Evyatar Segal <evyatarsegal1@gmail.com>
2023-01-09 23:52:41 +0200

    tried to add textbox

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,36 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+
       </main>
+      
+    <article class="regular">
+      <h2>Textbox</h2>
+
+      <label for="tbuser">Username:</label>
+      <input type="text" id="tbuser" placeholer="Enter Text"></input>
+
+      <input type="button" id="btn1" value="Click Me"></input>
+
+      <p class="output" id="output1"></p>
+
+
+    </article>
+
+    <script>
+      const txt1 = document.getElementById('tbuser');
+      const btn1 = documnet.getElementById('btn1');
+      const ou1 = document.getElementById('output1');
+      
+      function fun1() {
+        out1.innerHTML = txt1.value
+      }
+
+      btn1.addEventListener('click, fun1');
+
+    </script>
+
 
       <Footer />
     </div>
